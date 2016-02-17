2534517dbc99c896413e1af51f92be2cac00d8a2
Isak <isak.duner@gmail.com>
2016-02-17 12:17:58 +0100

    makefile changes

---
+++ sendpoint/libs/curl-build/tests/server/CMakeFiles/fake_ntlm.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/lib/base64.c" "/Users/Isak/Documents/projects/sendpoint/libs/curl-build/tests/server/CMakeFiles/fake_ntlm.dir/__/__/lib/base64.c.o"
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/lib/memdebug.c" "/Users/Isak/Documents/projects/sendpoint/libs/curl-build/tests/server/CMakeFiles/fake_ntlm.dir/__/__/lib/memdebug.c.o"
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/lib/mprintf.c" "/Users/Isak/Documents/projects/sendpoint/libs/curl-build/tests/server/CMakeFiles/fake_ntlm.dir/__/__/lib/mprintf.c.o"
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/lib/nonblock.c" "/Users/Isak/Documents/projects/sendpoint/libs/curl-build/tests/server/CMakeFiles/fake_ntlm.dir/__/__/lib/nonblock.c.o"
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/lib/strequal.c" "/Users/Isak/Documents/projects/sendpoint/libs/curl-build/tests/server/CMakeFiles/fake_ntlm.dir/__/__/lib/strequal.c.o"
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/lib/strtoofft.c" "/Users/Isak/Documents/projects/sendpoint/libs/curl-build/tests/server/CMakeFiles/fake_ntlm.dir/__/__/lib/strtoofft.c.o"
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/lib/timeval.c" "/Users/Isak/Documents/projects/sendpoint/libs/curl-build/tests/server/CMakeFiles/fake_ntlm.dir/__/__/lib/timeval.c.o"
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/lib/warnless.c" "/Users/Isak/Documents/projects/sendpoint/libs/curl-build/tests/server/CMakeFiles/fake_ntlm.dir/__/__/lib/warnless.c.o"
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/tests/server/fake_ntlm.c" "/Users/Isak/Documents/projects/sendpoint/libs/curl-build/tests/server/CMakeFiles/fake_ntlm.dir/fake_ntlm.c.o"
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/tests/server/getpart.c" "/Users/Isak/Documents/projects/sendpoint/libs/curl-build/tests/server/CMakeFiles/fake_ntlm.dir/getpart.c.o"
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/tests/server/util.c" "/Users/Isak/Documents/projects/sendpoint/libs/curl-build/tests/server/CMakeFiles/fake_ntlm.dir/util.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CURL_STATICLIB"
+  "HAVE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include/curl"
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/include"
+  "/Users/Isak/Documents/projects/sendpoint/libs/curl/lib"
+  "lib"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
